c2ee1b2794ad011f3d519aaff20ddad04e56cf76
Bob <150491881+stellar-security-os@users.noreply.github.com>
2025-01-04 00:33:49 +0100

    Validation added for Devices and Delete method.

--- app/Http/Controllers/V1/DeviceController.php
+++ app/Http/Controllers/V1/DeviceController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\DeviceLogin;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 
 class DeviceController extends Controller
 {
 
-    public function add(Request $request) {
+    public function add(Request $request): JsonResponse
+    {
 
         $identifier = $request->input('identifier');
         $name = $request->input('name');
@@ -25,21 +27,39 @@ class DeviceController extends Controller
 
     }
 
-    public function delete(Request $request)
+    public function delete(Request $request): JsonResponse
     {
-        DeviceLogin::where([['identifier', '=', $request->input('identifier')], ['name', '=', $request->input('name')]])->delete();
+
+        $identifier = $request->input('identifier');
+        $name = $request->input('name');
+
+        if($identifier === null || $name === null) {
+            return response()->json(['response_code' => 400, 'response_message' => 'Name or Identifier cannot be empty.']);
+        }
+
+        $device = DeviceLogin::where([['identifier', '=', $identifier], ['name', '=', $name]]);
+
+        if($device === null) {
+            return response()->json(['response_code' => 400, 'response_message' => 'Device not found.']);
+        }
+
+        $device->delete();
+
         return response()->json(['response_code' => 200]);
     }
 
 
 
-    public function devices(Request $request)
+    public function devices(Request $request): JsonResponse
     {
 
         $identifier = $request->input('identifier');
 
-        $deviceLogins = DeviceLogin::where('identifier', $identifier)->get();
+        if($identifier === null) {
+            return response()->json(['response_code' => 400, 'response_message' => 'Identifier cannot be empty.']);
+        }
 
+        $deviceLogins = DeviceLogin::where('identifier', $identifier)->get();
         return response()->json($deviceLogins);
 
     }
